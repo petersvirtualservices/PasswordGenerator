bca84ed8c2387abe11f50a06e46a951cad073538
Charlene <charlene@peters-services.com>
2021-09-05 20:36:16 -0500

    added push feature for arrays

--- script.js
+++ script.js
@@ -17,25 +17,22 @@ function generatePassword() {
   const lowercase = document.getElementById("#lowercase");
   const numbers = document.getElementById("#numbers");
   const length = document.getElementById("#length");
-  const lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const numbersAdd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const passwordArray = [];
 
-  if (uppercase) {
-    passwordArray.push(lettersUpper);
-  };
+    passwordArray.push(letters);
 
-  if (lowercase) {
     passwordArray.push(lettersLower);
-  }
 
-  if (numbers) {
     passwordArray.push(numbersAdd);
-  }
+
+  console.log(passwordArray);
+
 
   for ( var i = 0; i <= length; i++ ) {
-    var result = 'asdfA910 - This is a bogus passowrd, meant to be a placer.'
+    var result = 'asdfA910 - This is a bogus passowrd, meant to be a placer.';
     
     //Figure out why charAt isn't working.
     //passwordArray.charAt(Math.floor(Math.random()*passwordArray.length));
