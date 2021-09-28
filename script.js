b71311dafd46f8d09397f767c6af1fec6d1c10a8
Charlene <charlene@peters-services.com>
2021-09-27 19:00:00 -0500

    working on Math.random

--- script.js
+++ script.js
@@ -16,29 +16,27 @@ function generatePassword() {
   const uppercase = document.getElementById("#uppercase");
   const lowercase = document.getElementById("#lowercase");
   const numbers = document.getElementById("#numbers");
-  const length = document.getElementById("#length");
-  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  const lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  const numbersAdd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const long = document.getElementById("#long");
+  const letters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].splice(0);
+  const lettersLower = ["abcdefghijklmnopqrstuvwxyz"].splice(0);
+  const numbersAdd = ["0123456789"].splice(0);
   const passwordArray = [];
 
     passwordArray.push(letters);
-
     passwordArray.push(lettersLower);
-
     passwordArray.push(numbersAdd);
 
+
   console.log(passwordArray);
+  console.log(long);
 
+  for (var i = 0; i <= passwordArray.length; i++) {
+    var resultPre = passwordArray.toString();
 
-  for ( var i = 0; i <= length; i++ ) {
-    var result = 'asdfA910 - This is a bogus passowrd, meant to be a placer.';
+    var result = resultPre.charAt(Math.floor(Math.random()*26));
     
-    //Figure out why charAt isn't working.
-    //passwordArray.charAt(Math.floor(Math.random()*passwordArray.length));
+  }
 
-}
-
-return result;
+  return result;
 
 };
