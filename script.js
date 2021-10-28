35c6af702930e074c00b13be0bb779cc5211a30d
Charlene <charlene@peters-services.com>
2021-10-27 21:15:59 -0500

    updated password concatenation, added decoder and email option

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//email password using bcrypt
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  result = password;
+  //result = password;
   passwordText.value = password;
 }
 
@@ -13,30 +15,56 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  const uppercase = document.getElementById("#uppercase");
-  const lowercase = document.getElementById("#lowercase");
-  const numbers = document.getElementById("#numbers");
-  const long = document.getElementById("#long");
-  const letters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].splice(0);
-  const lettersLower = ["abcdefghijklmnopqrstuvwxyz"].splice(0);
-  const numbersAdd = ["0123456789"].splice(0);
-  const passwordArray = [];
+  const uppercase = document.getElementById("uppercase");
+  const lowercase = document.getElementById("lowercase");
+  const numbers = document.getElementById("numbers");
+  const long = document.getElementById("long");
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const numbersAdd = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var passwordArray = [];
+  var resultArray = [];
 
-    passwordArray.push(letters);
-    passwordArray.push(lettersLower);
-    passwordArray.push(numbersAdd);
-
-
-  console.log(passwordArray);
-  console.log(long);
-
-  for (var i = 0; i <= passwordArray.length; i++) {
-    var resultPre = passwordArray.toString();
-
-    var result = resultPre.charAt(Math.floor(Math.random()*26));
-    
+  if(uppercase.checked == true && lowercase.checked == true && numbers.checked == true){
+    var passwordArray = [...letters, ...lettersLower, ...numbersAdd];
   }
 
+  if(uppercase.checked == true && lowercase.checked == true && numbers.checked == false){
+    var passwordArray = [...letters, ...lettersLower];
+  }
+
+  if(uppercase.checked == true && lowercase.checked == false && numbers.checked == false){
+    var passwordArray = [...letters];
+  }
+
+  if(uppercase.checked == true && lowercase.checked == false && numbers.checked == true){
+    var passwordArray = [...letters, ...numbersAdd];
+  }
+
+  if(uppercase.checked == false && lowercase.checked == true && numbers.checked == true){
+    var passwordArray = [...lettersLower, ...numbersAdd];
+  }
+
+  if(uppercase.checked == false && lowercase.checked == true && numbers.checked == false){
+    var passwordArray = [...lettersLower];
+  }
+
+  if(uppercase.checked == false && lowercase.checked == false && numbers.checked == true){
+    var passwordArray = [...numbersAdd];
+  }
+  
+  var resultPre = passwordArray.toString();
+  var resultCommaRemoved = resultPre.split(',').join('');
+ 
+  var resultHere = resultCommaRemoved.charAt(Math.floor(Math.random()*passwordArray.length));
+
+  //Will not repeat password charAt
+  var resultOne = resultArray.push(resultHere);
+
+  
+  console.log(resultCommaRemoved);
+  console.log(resultHere);
+  console.log(resultOne);
   return result;
 
 };
