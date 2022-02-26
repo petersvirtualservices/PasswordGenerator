a9a7e1ad2dcb197037b4da0dd7f29261c02dc9b5
Charlene <charlene@peters-services.com>
2022-02-25 18:45:23 -0600

    completed password length

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //result = password;
+  result = password;
   passwordText.value = password;
 }
 
@@ -23,46 +23,53 @@ function generatePassword() {
   const lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const numbersAdd = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var passwordArray = [];
-  
-  if(uppercase.checked == true && lowercase.checked == true && numbers.checked == true){
+
+  if (uppercase.checked == true && lowercase.checked == true && numbers.checked == true) {
     var passwordArray = [...letters, ...lettersLower, ...numbersAdd];
   }
 
-  if(uppercase.checked == true && lowercase.checked == true && numbers.checked == false){
+  if (uppercase.checked == true && lowercase.checked == true && numbers.checked == false) {
     var passwordArray = [...letters, ...lettersLower];
   }
 
-  if(uppercase.checked == true && lowercase.checked == false && numbers.checked == false){
+  if (uppercase.checked == true && lowercase.checked == false && numbers.checked == false) {
     var passwordArray = [...letters];
   }
 
-  if(uppercase.checked == true && lowercase.checked == false && numbers.checked == true){
+  if (uppercase.checked == true && lowercase.checked == false && numbers.checked == true) {
     var passwordArray = [...letters, ...numbersAdd];
   }
 
-  if(uppercase.checked == false && lowercase.checked == true && numbers.checked == true){
+  if (uppercase.checked == false && lowercase.checked == true && numbers.checked == true) {
     var passwordArray = [...lettersLower, ...numbersAdd];
   }
 
-  if(uppercase.checked == false && lowercase.checked == true && numbers.checked == false){
+  if (uppercase.checked == false && lowercase.checked == true && numbers.checked == false) {
     var passwordArray = [...lettersLower];
   }
 
-  if(uppercase.checked == false && lowercase.checked == false && numbers.checked == true){
+  if (uppercase.checked == false && lowercase.checked == false && numbers.checked == true) {
     var passwordArray = [...numbersAdd];
   }
-  
+
   var resultPre = passwordArray.toString();
   var resultCommaRemoved = resultPre.split(',').join('');
-  
-  let resultArray = [];
-  var resultHere = resultArray.push(resultCommaRemoved.charAt(Math.floor(Math.random()*passwordArray.length)));
-  
-  var result = resultHere;
-
-  
   console.log(resultCommaRemoved);
-  console.log(resultHere);
+
+  let resultArray = [];
+
+  for (var i = 0; i < long.value; i++) {
+    var resultEntry = resultCommaRemoved.charAt(Math.floor(Math.random() * resultCommaRemoved.length));
+    resultArray.push(resultEntry);
+  }
+
+
+  var resultAlmost = resultArray.toString();
+  var result = resultAlmost.split(',').join('');
+
+
+  //console.log(resultCommaRemoved);
+  console.log(resultArray);
   return result;
 
 };
